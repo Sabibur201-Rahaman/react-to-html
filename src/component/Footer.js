bbc8791d35014610055c6afb4572b7fb1cb1b809
Sabibur201-Rahaman <107191991+Sabibur201-Rahaman@users.noreply.github.com>
2023-02-22 17:29:40 +0530

    all files are updated

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
                 >Name<sup>*</sup></label
               >
               <div class="col-sm-9">
-                <div></div>
+                <div> </div>
                 <input
                   type="text"
                   class="form-control"
