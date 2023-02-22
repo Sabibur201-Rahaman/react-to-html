8aa7c7ea9c774e9d50db501ddee8e42e331cf472
Sabibur201-Rahaman <107191991+Sabibur201-Rahaman@users.noreply.github.com>
2023-02-22 16:18:11 +0530

    html to react convert

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Description from "./component/Description";
+import Menu from "./component/Menu";
+import StartHeader from "./component/StartHeader";
+import Speakers from "./component/Speakers";
+import Tickets from "./component/Tickets";
+import Schedule from "./component/Schedule";
+import Footer from "./component/Footer";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <main>
+    <section className='container'>
+  <Menu/>
+  <StartHeader/>
+  </section>
+  <Description/>
+  <Speakers/>
+  <Tickets/>
+  <Schedule/>
+  <Footer/>
+  </main>
+  )
 }
-
 export default App;
